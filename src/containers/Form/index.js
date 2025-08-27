77780e5cb3d687219256b2e541bc79ed4fa0d81f
Jord35 <jordan.gillouaye@gmail.com>
2025-08-27 21:18:37 +0200

    appel de onSuccess après un envoi réussi pour afficher la modal de confirmation

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // declanche "message envoyé"
       } catch (err) {
         setSending(false);
         onError(err);
